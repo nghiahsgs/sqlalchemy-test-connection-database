56ab3727969022ac51be441280ba4deedc8e822b
nghiahsgs <nghiahsgs@gmail.com>
2022-07-07 10:39:35 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,16 +1,16 @@
 from utils_db import *
 
-try:
-    db = next(get_db())
+#try:
+db = next(get_db())
 
-    # L = db.query(Bank_bill).all()
-    # print(len(L))
+# L = db.query(Bank_bill).all()
+# print(len(L))
 
-    result = db.execute("Show tables;")
+result = db.execute("Show tables;")
 
-    print(db)
-    db.close()
+print(db)
+db.close()
 
-    print("Everything is ok")
-except Exception as e:
-    print("Da co loi xay ra, co the db connection bi sai !")
+print("Everything is ok")
+#except Exception as e:
+#    print("Da co loi xay ra, co the db connection bi sai !")
